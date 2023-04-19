b564d9ee23de12bc67668c00f013618c41712685
BroDah0 <alan.drg11@gmail.com>
2023-04-19 13:32:44 +0200

    ia improved

--- Assets/Scripts/IAennemy.cs
+++ Assets/Scripts/IAennemy.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using UnityEditor;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class IAennemy : MonoBehaviour
 {
@@ -12,9 +13,11 @@ public class IAennemy : MonoBehaviour
     Queue<Vector3> posPlayer = new Queue<Vector3>();
     Queue<bool> flipPlayer = new Queue<bool>();
     bool canFollow = false;
+    bool Init = true;
     // Start is called before the first frame update
     void Start()
     {
+        
         Invoke("Waiting", 1.5f);
     }
 
@@ -25,13 +28,20 @@ public class IAennemy : MonoBehaviour
         flipPlayer.Enqueue(Player.GetComponent<SpriteRenderer>().flipX);
         if (canFollow)
         {
+            
             transform.position = posPlayer.Dequeue();
             GetComponent<SpriteRenderer>().flipX = flipPlayer.Dequeue();
+        }
+        if (Init)
+        {
+            transform.Translate((Player.transform.position - transform.position)* 2.5f * Time.deltaTime, Space.World);
         }
     }
 
     void Waiting()
     {
+        Init = false;
         canFollow = true;
+
     }
 }
