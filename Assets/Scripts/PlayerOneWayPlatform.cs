0862485746c1c6ab1695a2cdeb2c5a7309148c39
BroDah0 <alan.drg11@gmail.com>
2023-05-08 22:52:03 +0200

    one way platform

---
+++ Assets/Scripts/PlayerOneWayPlatform.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerOneWayPlatform : MonoBehaviour
+{
+    private GameObject currentOneWayPlatform;
+
+    [SerializeField] private BoxCollider2D playerCollider;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)
+        {
+            if (currentOneWayPlatform == null)
+            {
+                StartCoroutine(DisableCollision());
+            }
+        }
+                             
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("OneWayPlatform"))
+        {
+            currentOneWayPlatform = collision.gameObject;   
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("OneWayPlatform"))
+        {
+            currentOneWayPlatform = null;
+        }
+    }
+
+    private IEnumerable DisableCollision()
+    {
+        BoxCollider2D platformCollider = currentOneWayPlatform.GetComponent<BoxCollider2D>();
+
+        Physics2D.IgnoreCollision(playerCollider, platformCollider);
+        yield return new WaitForSeconds(0.25f);
+        Physics2D.IgnoreCollision(playerCollider, platformCollider, false);
+    }
+}
